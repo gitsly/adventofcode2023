84bc894f4d9ec9086b9eba227f44110fc91c688b
Martin Collberg <martin.collberg@gmail.se>
2023-12-03 12:52:17 +0100

    Start parsing input

---
+++ src/adventofcode2023/day2.clj
@@ -0,0 +1,49 @@
+(ns adventofcode2023.day2
+  (:require [clojure.string :as str]
+            [adventofcode2023.utils :as u]))
+
+
+(def input (u/get-lines "resources/day2.pt1.txt"))
+
+(let [sample-line "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+
+      parse-cube
+      ;; Parse a single cube (gives count and color)
+      (fn[s]
+        (let [[cnt color] (str/split (str/trim s) #" ")]
+          {:count (Integer/parseInt cnt)
+           :color (keyword color)}))
+
+      parse-set (fn[s]
+                  (let [cubes (str/split s #",")]
+                    (map parse-cube cubes)))
+
+      parse-sets (fn[s]
+                   (let [sets (str/split s #";")]
+                     (count sets)))
+
+      game (re-find #"Game (\d):" sample-line)
+
+      parse-game (fn[s]
+                   (let [r (re-find #"Game (\d):.*" s)]
+                     r
+                     ))
+
+      parse-game (fn[s]
+                   (let [[_ id] 
+                         (re-find #"Game (\d):" s)]
+                     {:id (Integer/parseInt id)}))
+      ]
+
+  (re-find #"Game (\d+):" sample-line)
+  (parse-game sample-line)
+
+  ;;  (parse-set "3 blue, 4 red")
+
+
+  ;;(parse-cube "3 blue")
+  
+
+  ;;  (parse-sets "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+
+  )
