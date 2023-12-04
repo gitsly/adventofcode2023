295e2ddd19e996a544479aff0b7c65709ae25d09
Martin Collberg <martin.collberg@gmail.se>
2023-12-04 23:57:30 +0100

    parsing a line

--- src/adventofcode2023/day3.clj
+++ src/adventofcode2023/day3.clj
@@ -2,3 +2,24 @@
   (:require [clojure.string :as str]
             [adventofcode2023.utils :as u]))
 
+
+(def input (u/get-lines "resources/day3.sample.txt"))
+
+(let [parse-char (fn [ch]
+                   (let [ch (str ch)]
+                     (cond
+                       (re-matches #"\." ch) nil
+                       (re-matches #"\d" ch) ch)))
+
+      parse-line (fn[line
+                     y]
+                   (for [[ch x] (zipmap line (range))]
+                     {:x x
+                      :y y
+                      :char (parse-char ch)}))]
+
+  (parse-line (first input) 0)
+
+
+  )
+
