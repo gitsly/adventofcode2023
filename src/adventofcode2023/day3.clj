357c22b7c489bf2094400039ab06929a461f47b0
Martin Collberg <martin.collberg@gmail.com>
2023-12-12 21:34:29 +0100

    working on transforming the data to let the items contain everything

--- src/adventofcode2023/day3.clj
+++ src/adventofcode2023/day3.clj
@@ -4,40 +4,21 @@
 
 
 (def input (u/get-lines "resources/day3.sample.txt"))
-
 (def input (u/get-lines "resources/day3.txt"))
 
-(def input ["617......T"
-            ".......58."
-            ".52¤1#23.."
-            "......755."])
-
 (def input [".........."
             ".........."
             ".........."
             ".........."
             ".........."])
-
-(def input ["...1......"
-            "..1*1....."
-            "...1......"
-            ".........."
-            ".........."])
-
-(def input [".........."
-            ".........."
-            ".........1"
-            "........*."
-            ".......123"])
-
-
 
 (map println input)
 
 (let [parse-char (fn [ch]
                    (let [ch-as-str (str ch)]
                      (cond
-                       (re-matches #"[^\d^\.]" ch-as-str) {:sym ch }
+                       (re-matches #"[^\d^\.^\*]" ch-as-str) {:sym ch }
+                       (re-matches #"\*" ch-as-str ) {:sym :gear }
                        (re-matches #"\." ch-as-str) nil
                        (re-matches #"\d" ch-as-str) ch
                        )))
@@ -107,12 +88,17 @@
                            (filter #(nil? (first %))
                                    (group-by :grp items))] v))
 
+      get-number (fn [items]
+                   (Integer/parseInt
+                    (apply str
+                           (map :char 
+                                items))))
+
       process-grp (fn[item-list
                       symbols]
-                    (let [num (Integer/parseInt
-                               (apply str
-                                      (map :char 
-                                           item-list)))
+                    (let [num (get-number item-list)
+
+                          
 
                           adjacent (some true? (for [ch item-list
                                                      sy symbols]
@@ -126,15 +112,47 @@
                                   (flatten
                                    (map #(vec  %) input))))
 
+      is-number? (fn [item] (re-matches #"\d" (str (:char item))))
+
+      number-items (filter is-number? items)
+
+      ;; A gear is any * symbol that is adjacent to exactly two part numbers.
+      is-star?(fn[item] (= :gear (-> item :char :sym )))
+
+      stars (filter is-star? items)
+
+      get-adjacent (fn [item
+                        col-to-check]
+                     (filter #(is-adjacent? item %) col-to-check))
+
+      ;; finds numbers adjacent to a symbol and sum them up.
+      pt-1 (reduce + 
+                   (map :num
+                        (filter :adjacent 
+                                (map #(process-grp % symbols) number-groups))))
+
+      add-numbers (fn[items]
+                    "Decorates the items with their associated number in :num (if any)"
+                    (let [number-groups (group-by :grp
+                                                  (filter is-number? items))
+                          grp-to-number (into (hash-map)
+                                              (for [[k v] number-groups]
+                                                [k (get-number v)]))
+
+                          trans (fn [item]
+                                  (let [grp (:grp item)]
+                                    (if grp
+                                      (assoc item :num 
+                                             (grp grp-to-number))
+                                      item)))
+                          ]
+                      (map trans items)))
       ] 
 
-  ;; finds numbers adjacent to a symbol and sum them up.
-  (reduce + 
-          (map :num
-               (filter :adjacent 
-                       (map #(process-grp % symbols)number-groups)))) ; 1110045 your answer is too high
+  (map #(select-keys % [:char :num]) (add-numbers items)))
 
-  )
+  ) 
+)
 ;;-- 531932 (with better key gen-) Correct!
 
 
