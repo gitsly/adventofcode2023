fd4042147b01d6f7df2fe3597870102a89f2f9f1
Martin Collberg <martin.collberg@gmail.com>
2023-12-16 16:17:57 +0100

    parsing input

--- src/adventofcode2023/day4.clj
+++ src/adventofcode2023/day4.clj
@@ -3,3 +3,25 @@
             [adventofcode2023.utils :as u]))
 
 (def input (u/get-lines "resources/day4.sample.txt"))
+(def input (u/get-lines "resources/day4.sample.txt"))
+
+
+(let [str  "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+
+
+
+      ]
+  (let [to-int-vec (fn[s]
+                     (map
+                      #(Integer/parseInt %)
+                      (str/split (str/trim s) #"\ ")))
+
+        [left right] (str/split str #"\|" )
+        [cardinfo winning] (str/split str #":" )]
+
+
+    (to-int-vec right)
+
+    ))
+
+
