938cfeca3a757f0f37956bd0d7a9e82e024def20
Martin Collberg <martin.collberg@gmail.com>
2023-12-25 13:22:12 +0100

    mapping works

--- src/adventofcode2023/day5.clj
+++ src/adventofcode2023/day5.clj
@@ -21,13 +21,29 @@
                     (let [sections (filter #(not(= '("") %))
                                            (partition-by #(= "" %) lines))
 
+                          parse-mapping-line (fn[line]
+                                               (let [get-range (fn[start length] (map #(+ start %) (range length)))
+                                                     [dest-range-start source-range-start range-length]
+                                                     (map #(Integer/parseInt %) (str/split line #" "))]
+
+                                                 (zipmap
+                                                  (get-range source-range-start range-length)
+                                                  (get-range dest-range-start range-length))
+
+                                                 ))
+
+                          ;; 98 -> 50
+                          ;; 99 -> 51
+
                           [seeds & sections] sections
                           parse-section (fn [section]
                                           (let [[_ from to] (re-find  #"(.*)-to-(.*) map" (first section))
                                                 mappings (rest section)
                                                 header {:from (keyword from)
                                                         :to (keyword to)}]
-                                            header))
+
+                                            (parse-mapping-line (first mappings))
+                                            ))
 
 
                           ]
@@ -37,5 +53,6 @@
 
       ]
 
-  (parse-input input)
+  ((parse-input input) 99)
   )
+
