e1272a6691dda787fe0540fe85a05cbb696a3a34
Martin Collberg <martin.collberg@gmail.com>
2023-12-25 12:23:26 +0100

    scaffolding for day5

---
+++ src/adventofcode2023/day5.clj
@@ -0,0 +1,14 @@
+(ns adventofcode2023.day5
+  (:require [clojure.string :as str]
+            [adventofcode2023.utils :as u]
+            [clojure.set :as set]))
+
+(def input (u/get-lines "resources/day5.sample.txt"))
+;; (def input (u/get-lines "resources/day5.txt"))
+
+(map println input)
+
+(let []
+
+  )
+
